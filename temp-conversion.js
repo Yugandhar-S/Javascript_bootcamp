bc36e4eef4b0076fba24d821814367467f5ebcec
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-05-11 00:43:26 +0530

    Add files via upload

---
+++ temp-conversion.js
@@ -0,0 +1,10 @@
+let fahrenheit = 50;
+
+//calculate celsius and store in celsius var, print it
+//calcualte in kelvin, store it in kelvin var, print it
+
+let celsius = ((fahrenheit - 32) * 5) / 9;
+
+let kelvin = ((fahrenheit + 459.67) * 5) / 9;
+
+console.log(fahrenheit, celsius, kelvin);

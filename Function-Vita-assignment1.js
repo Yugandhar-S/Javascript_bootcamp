6c28a84aba645f4aab650e89b1f5d82a393a0920
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-05-11 18:11:56 +0530

    Add files via upload

---
+++ Function-Vita-assignment1.js
@@ -0,0 +1,12 @@
+let max = function (num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    return `The max number is ${num1}`;
+  } else if (num2 > num3 && num2 > num1) {
+    return `The max number is ${num2}`;
+  } else {
+    return `The max number is ${num3}`;
+  }
+};
+
+let maxNumber = max(4, 6, 3);
+console.log(maxNumber);

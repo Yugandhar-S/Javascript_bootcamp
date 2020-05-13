11e6280e5e6fb82f172a976709e90789b7f3a3c1
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-05-13 20:56:22 +0530

    Add files via upload

--- expense-tracker.js
+++ expense-tracker.js
@@ -3,9 +3,16 @@
 //addExpense -> description, amount
 //getAccountSummary-> total up all expenses -> Yugandhar has $2362 in expenses
 
+//add income array to account
+//setup and addIncome method->description,amount
+//tweak getAccountSummary
+// return `${this.name} has $${totalExpenses} in expenses.`;
+//Yugandhar Salvi has a balance of $10. $100 in income $90 in expenses.
+
 const account = {
   name: "Yugandhar Salvi",
   expenses: [],
+  income: [],
 
   addExpense: function (description, amount) {
     this.expenses.push({
@@ -14,16 +21,34 @@ const account = {
     });
   },
 
+  addIncome: function (source, income) {
+    this.income.push({
+      Description: source,
+      Income: income,
+    });
+  },
+
   getAccountSummary: function () {
     let totalExpenses = 0;
     this.expenses.forEach(function (expense) {
       totalExpenses = totalExpenses + expense.Amount;
     });
-    return `${this.name} has $${totalExpenses} in expenses.`;
+
+    let totalIncome = 0;
+    this.income.forEach(function (amount) {
+      totalIncome = totalIncome + amount.Income;
+    });
+
+    return `${this.name} has balance of $${
+      totalIncome - totalExpenses
+    }. $${totalIncome} in income.$${totalExpenses}in expenses.`;
   },
 };
 
-account.addExpense("Rent", 950);
-account.addExpense("Coffee", 2);
-console.log(account.expenses);
+account.addExpense("Rent", 45);
+account.addExpense("Coffee", 45);
+account.addIncome("Job", 100);
+console.log(account.income);
 console.log(account.getAccountSummary());
+
+//addIncome("Cashback", 2000);

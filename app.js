2ba287b97780395f00ae11ace4762ff5f61785e8
Yugandhar Shashikant Salvi <60753349+Yugandhar-S@users.noreply.github.com>
2020-06-07 00:38:22 +0530

    Fixed an Edge Case
    Addressed an edge case with JavaScript code.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const puzzleEl = document.querySelector("#puzzle");
 const guessesEl = document.querySelector("#guesses");
-let game1 = new Hangman("Cat", 2);
+let game1 = new Hangman("Car Parts", 2);
 
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = game1.remainingGuesses;
+puzzleEl.textContent = game1.puzzle;
+guessesEl.textContent = game1.statusMessage;
 console.log(game1.status);
 
 window.addEventListener("keypress", (e) => {
   let guess = String.fromCharCode(e.charCode);
   game1.makeGuess(guess);
-  puzzleEl.textContent = game1.getPuzzle();
-  guessesEl.textContent = game1.remainingGuesses;
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = game1.statusMessage;
   console.log(game1.status);
 });
--- hangman.js
+++ hangman.js
@@ -7,8 +7,8 @@ class Hangman {
   }
 
   calculateStatus() {
-    const finished = this.word.every((letter) =>
-      this.guessedLetters.includes(letter)
+    const finished = this.word.every(
+      (letter) => this.guessedLetters.includes(letter) || letter === " "
     );
 
     if (this.remainingGuesses === 0) {
@@ -20,7 +20,7 @@ class Hangman {
     }
   }
 
-  getStatusMessage() {
+  get statusMessage() {
     if (this.status === "playing") {
       return `Guesses left : ${this.remainingGuesses}`;
     } else if (this.status === "failed") {
@@ -30,7 +30,7 @@ class Hangman {
     }
   }
 
-  getPuzzle() {
+  get puzzle() {
     let puzzle = "";
 
     this.word.forEach((letter) => {
